c89a354fc02ed13e177fddcb3c6653aac54a7bcf
tee <teeomotunde@yahoo.com>
2023-04-02 18:43:24 -0400

    terraform code

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "aws_security_group" "sg1" {
+  name = "webserver"
+  # id = "sg-0c0c5a15156f085b9"
+
+}
---
+++ ec2.tf
@@ -0,0 +1,16 @@
+
+
+resource "aws_instance" "demo1" {
+  ami                    = var.ami-type
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.sg1.id]
+  key_name               = "classkey"
+  user_data              = file("${path.module}/postgresql.sh")
+  tags = {
+    "Name" = var.instance-name
+    "env"  = var.env
+    "Team" = var.team
+
+  }
+}
+
